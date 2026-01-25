75e689a870e0508ca29918f651ee91f87aaad1aa
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-24 23:50:48 -0300

    feat : create class password hasher

---
+++ src/AuthBoss.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using AuthBoss.Domain.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthBoss.Infrastructure.Security;
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        throw new NotImplementedException();
+    }
+}
