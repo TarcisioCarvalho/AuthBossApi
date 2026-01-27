557402dbe99b01294967b1393d140ed02374f433
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-26 21:25:44 -0300

    feat : implements feature of Generate Hash and feature Veirfy Hash

--- src/AuthBoss.Infrastructure/Security/PasswordHasher.cs
+++ src/AuthBoss.Infrastructure/Security/PasswordHasher.cs
@@ -1,4 +1,6 @@
 ﻿using AuthBoss.Domain.Security;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace AuthBoss.Infrastructure.Security;
 public class PasswordHasher : IPasswordHasher
@@ -7,11 +9,29 @@ public class PasswordHasher : IPasswordHasher
     {
         var salt = Guid.NewGuid().ToString();
         var passwordWithSalt = password + salt;
-        throw new NotImplementedException();
+        string passwordHashed = passwordWithSalt;
+        long iteractions = 1000;
+
+        for (int i = 0; i < iteractions; i++)
+        {
+            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(passwordHashed));
+            passwordHashed = Convert.ToHexString(bytes);
+        }
+
+        return $"{iteractions}${salt}${passwordHashed}";
     }
 
     public bool Verify(string password, string hash)
     {
-        throw new NotImplementedException();
+        long iteractions = Convert.ToInt64(hash.Split('$')[0]);
+        string salt = hash.Split('$')[1];
+        var passwordWithSalt = password + salt;
+        string passwordHashed = passwordWithSalt;
+        for (int i = 0; i < iteractions; i++)
+        {
+            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(passwordHashed));
+            passwordHashed = Convert.ToHexString(bytes);
+        }
+       return passwordHashed == hash.Split('$')[2];
     }
 }
