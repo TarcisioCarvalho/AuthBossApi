6c2d78c02a8215d59ed2acb2661570108f92a293
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-01-28 23:31:33 -0300

    feat: configure the project with the libs and create the first success test.

--- tests/AuthBoss.Infrastructure.Tests/Security/PasswordHasherTest.cs
+++ tests/AuthBoss.Infrastructure.Tests/Security/PasswordHasherTest.cs
@@ -1,4 +1,15 @@
-﻿namespace AuthBoss.Infrastructure.Tests.Security;
+﻿using AuthBoss.Infrastructure.Security;
+using FluentAssertions;
+using Xunit;
+
+namespace AuthBoss.Infrastructure.Tests.Security;
 public class PasswordHasherTest
 {
+    [Fact]
+    public void Success()
+    {
+        string passwordTest = "password";
+        var hash = new PasswordHasher().Generate(passwordTest);
+        hash.Should().NotBeNull();
+    }
 }
