daa04617eeeaaf5aeaadbcd689c4b51dee5b4450
TarcisioCarvalho <tarcisiokta2012@gmail.com>
2026-02-14 23:03:05 -0300

    feat :  begin with controller and register endpoint

---
+++ src/AuthBoss.API/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using AuthBoss.Communication.Requests.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthBoss.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Register([FromBody] RequestRegisterUserJson request)
+    {
+        return Ok();
+    }
+}
---
+++ src/AuthBoss.Comunication/Requests/User/RequestRegisterUserJson.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthBoss.Communication.Requests.User;
+public class RequestRegisterUserJson
+{
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    [Required]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    public string Password { get; set; } = string.Empty;
+    [Required]
+    public DateTime BirthDate { get; set; }
+}
